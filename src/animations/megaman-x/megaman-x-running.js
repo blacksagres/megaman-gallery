7fa96cdcca0eab7fbccb4bd500ceb895445ee5d0
Blacksagres <geraldo.nogueirabs@gmail.com>
2020-05-12 19:39:08 +0200

    Promisify animation load

--- src/animations/megaman-x/megaman-x-running.js
+++ src/animations/megaman-x/megaman-x-running.js
@@ -1,36 +1,35 @@
-import * as PIXI from 'pixi.js';
+import * as PIXI from "pixi.js";
 
-export const megamanXStartStage = (pixiApp) => {
+export const megamanXRunning = (pixiApp) => {
   return new Promise((resolve, reject) => {
-    const doneLoading = (loopAnimation) => {
-
+    const doneLoading = () => {
       // https://www.youtube.com/watch?v=GKre-3pBQac
       // https://www.html5gamedevs.com/topic/25619-is-this-an-efficient-way-to-load-sprites/
-  
+
       const frames = [];
-  
-      for (var i = 0; i <= 12; i++) {
+
+      for (var i = 0; i <= 9; i++) {
         // magically works since the spritesheet was loaded with the pixi loader
-        frames.push(PIXI.Texture.from('megaman-x-start-stage' + i + '.png'));
+        frames.push(PIXI.Texture.from("megaman-x-running" + i + ".png"));
       }
-  
+
       const sectionSprite = new PIXI.AnimatedSprite(frames);
-  
+
       sectionSprite.scale.set(3);
       sectionSprite.anchor.set(0.5);
       sectionSprite.x = pixiApp.view.width / 2;
       sectionSprite.y = pixiApp.view.height / 2;
-  
+
       pixiApp.stage.addChild(sectionSprite);
-  
+
       sectionSprite.animationSpeed = 0.2;
       sectionSprite.loop = false;
-  
+
       sectionSprite.play();
       resolve(sectionSprite);
     };
-   
-    pixiApp.loader.add('../../sprites/megaman-x/megaman-x-start-stage.json');
+
+    pixiApp.loader.add("../../sprites/megaman-x/megaman-x-running.json");
     pixiApp.loader.load(doneLoading);
-  })
-}
+  });
+};
--- src/components/main-canvas.js
+++ src/components/main-canvas.js
@@ -1,23 +1,21 @@
-import * as PIXI from 'pixi.js';
-import { megamanXStartStage } from '../animations/megaman-x/megaman-x-start-stage';
+import * as PIXI from "pixi.js";
+import { megamanXStartStage } from "../animations/megaman-x/megaman-x-start-stage";
+import { megamanXRunning } from "../animations/megaman-x/megaman-x-running";
 
 // https://www.piskelapp.com/p/agxzfnBpc2tlbC1hcHByEwsSBlBpc2tlbBiAgKC9_OjjCQw/edit
 export default function MainCanvas() {
-  const app =
-    new PIXI.Application({
-      height: window.innerHeight,
-      width: window.innerWidth,
-      view: document.querySelector('#main-canvas')
-    });
-
-  let animation = megamanXStartStage(app);
-
-
-  document.querySelector('#control-animation-loop').addEventListener('change', (event) => {
-    animation.loop = event.target.checked;
-    animation.play();
+  const app = new PIXI.Application({
+    height: window.innerHeight,
+    width: window.innerWidth,
+    view: document.querySelector("#main-canvas"),
   });
 
-  
-
+  megamanXStartStage(app).then((animation) => {
+    document
+      .querySelector("#control-animation-loop")
+      .addEventListener("change", (event) => {
+        animation.loop = event.target.checked;
+        animation.play();
+      });
+  });
 }
