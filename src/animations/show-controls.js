f0ae0e85ac6da8742ed7b0f0d26279102c572f41
Blacksagres <geraldo.nogueirabs@gmail.com>
2020-05-16 15:38:02 +0200

    Remove duration

--- src/animations/show-controls.js
+++ src/animations/show-controls.js
@@ -7,7 +7,6 @@ const closeControlsPanel = document.querySelector("#close-controls-panel");
 closeControlsPanel.addEventListener("click", () => {
   gsap
     .timeline()
-    // .duration(.3)
     .to(controlsContainer, {
       opacity: 0,
       ease: Power3.easeIn,
@@ -31,26 +30,22 @@ closeControlsPanel.addEventListener("click", () => {
 menuToggleButton.addEventListener("click", () => {
   gsap
     .timeline()
-    // .duration(.3)
     .to(menuToggleButton, {
       opacity: 0,
       translateY: "-10px",
       ease: Power3.easeIn,
       zIndex: 0,
-      duration: 0.3,
     })
     .fromTo(
       controlsContainer,
       {
         translateY: "10px",
         ease: Power3.easeIn,
-        duration: 0.3,
       },
       {
         translateY: "0px",
         opacity: 1,
         ease: Power3.easeIn,
-        duration: 0.3,
       }
     );
 });
