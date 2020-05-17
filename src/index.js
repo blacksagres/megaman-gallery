b7798f1d0b5654f99f4f8af4f55bb1cca7e08699
Blacksagres <geraldo.nogueirabs@gmail.com>
2020-05-17 09:55:45 +0200

    Load image svg on runtime - after build

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
-import MainCanvas from './components/main-canvas';
-import './styles/index.scss';
+import MainCanvas from "./components/main-canvas";
+import "./styles/index.scss";
 
-import './components/game-selection';
-import './animations/show-controls';
+import "./components/game-selection";
+import "./animations/show-controls";
 
+const loadReloadIcon = () => {
+  document
+    .querySelector("#repeat-icon")
+    .setAttribute("src", "./icons/repeat-icon.svg");
+};
+
+loadReloadIcon();
 MainCanvas();
