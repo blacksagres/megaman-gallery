8e94e62c336b607208642855922ebf80c0fcc384
Blacksagres <geraldo.nogueirabs@gmail.com>
2020-05-17 10:33:53 +0200

    Ah ios - we meet again

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import "./styles/index.scss";
 import "./components/game-selection";
 import "./animations/show-controls";
 
+import './wd-40/silly-silly-ios';
+
 const loadReloadIcon = () => {
   document
     .querySelector("#repeat-icon")
---
+++ src/wd-40/silly-silly-ios.js
@@ -0,0 +1,8 @@
+const resize = () => {
+  // We execute the same script as before
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+};
+
+window.addEventListener('resize', resize);
+resize();
