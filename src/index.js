f14cdfebda1fb39ff9c42725380406b95e47b961
Blacksagres <geraldo.nogueirabs@gmail.com>
2021-12-12 18:06:33 +0100

    Neatify button, clean css

--- src/index.js
+++ src/index.js
@@ -1,16 +1,10 @@
-import MainCanvas from "./components/main-canvas";
 import "./styles/index.scss";
 
 import "./components/game-selection";
 import "./animations/show-controls";
 
-import './wd-40/silly-silly-ios';
+import "./wd-40/silly-silly-ios";
 
-const loadReloadIcon = () => {
-  document
-    .querySelector("#repeat-icon")
-    .setAttribute("src", "./icons/repeat-icon.svg");
-};
+import MainCanvas from "./components/main-canvas";
 
-loadReloadIcon();
 MainCanvas();
