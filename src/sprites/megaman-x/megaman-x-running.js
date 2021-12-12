360d11822741417905de8ff096b814870c904d74
Blacksagres <geraldo.nogueirabs@gmail.com>
2021-12-12 17:54:49 +0100

    Refactor other loaders

--- src/sprites/megaman-x/megaman-x-running.js
+++ src/sprites/megaman-x/megaman-x-running.js
@@ -1,36 +1,12 @@
 import * as PIXI from "pixi.js";
+import { loadSprite } from "../sprite-sheet-loader";
 
 export const megamanXRunning = (pixiApp) => {
-  return new Promise((resolve, reject) => {
-    const doneLoading = () => {
-      // https://www.youtube.com/watch?v=GKre-3pBQac
-      // https://www.html5gamedevs.com/topic/25619-is-this-an-efficient-way-to-load-sprites/
-
-      const frames = [];
-
-      for (var i = 0; i <= 9; i++) {
-        // magically works since the spritesheet was loaded with the pixi loader
-        frames.push(PIXI.Texture.from("megaman-x-running" + i + ".png"));
-      }
-
-      const sectionSprite = new PIXI.AnimatedSprite(frames);
-
-      sectionSprite.scale.set(3);
-      sectionSprite.anchor.set(0.5);
-      sectionSprite.x = pixiApp.view.width / 2;
-      sectionSprite.y = pixiApp.view.height / 2;
-
-      pixiApp.stage.addChild(sectionSprite);
-
-      sectionSprite.animationSpeed = 0.2;
-      sectionSprite.loop = false;
-
-      sectionSprite.play();
-      resolve(sectionSprite);
-    };
-
-    const resource = "../../sprites/megaman-x/megaman-x-running.json";
-    if (!pixiApp.loader.resources[resource]) pixiApp.loader.add(resource);
-    pixiApp.loader.load(doneLoading);
+  return loadSprite({
+    pixiApp,
+    textureName: "megaman-x-running",
+    nrOfSprites: 9,
+    resourcePath:
+      window.location.origin + "/sprites/megaman-x/megaman-x-running.json",
   });
 };
